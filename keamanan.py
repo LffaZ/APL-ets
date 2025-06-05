08746592a733c5f8021e5adcbcb4a95547816112
elinaaahhh <putrielin4105@gmail.com>
2025-06-05 15:30:26 +0700

    add hash, verif

--- keamanan.py
+++ keamanan.py
@@ -1,2 +1,31 @@
 # hashing password 
 # verifikasi pin sebelum transaksi
+# security.py
+import hashlib
+import getpass
+
+class Security:
+    def _init_(self, pin_plain):
+        """
+        Saat objek dibuat, langsung hash dan simpan PIN-nya.
+        """
+        self.hashed_pin = self.hash_pin(pin_plain)
+
+    def hash_pin(self, pin):
+        """
+        Hashing PIN pakai SHA-256.
+        """
+        return hashlib.sha256(pin.encode()).hexdigest()
+
+    def verifikasi(self):
+        """
+        Minta input PIN, lalu cocokkan dengan hashed_pin yang disimpan.
+        """
+        print("\n--- Verifikasi PIN ---")
+        pin_input = getpass.getpass("Masukkan PIN Anda: ")
+        if self.hash_pin(pin_input) == self.hashed_pin:
+            print("PIN benar. Akses transaksi diberikan.\n")
+            return True
+        else:
+            print("PIN salah. Transaksi dibatalkan.\n")
+            return False
