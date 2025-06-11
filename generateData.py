210325caef91e51fa47409d9001f389e357935e8
Alifah Zuhrah <alifah.zuhrah@gmail.com>
2025-06-11 21:00:58 +0700

    fix: handling failed login

--- generateData.py
+++ generateData.py
@@ -4,14 +4,10 @@ from datetime import datetime
 from filelog import FileLogger
 import faker
 
-# Setup
 fake = faker.Faker('id_ID')
 folder_name = "data_atm"
-
-# Buat folder jika belum ada
 os.makedirs(folder_name, exist_ok=True)
 
-# Dummy data
 num_users = 30
 user_data = []
 saldo_data = []
@@ -21,10 +17,10 @@ for i in range(1, num_users + 1):
     user_id = random.randint(10**9, 10**10 - 1)
     pin = random.randint(1000, 9999)
     nama = fake.name()
-    nama_ibu = fake.first_name_female()
     saldo = random.randint(100_000, 10_000_000)
+    login_fail = 0
 
-    user_data.append([user_id, pin, nama, nama_ibu, saldo])
+    user_data.append([user_id, pin, nama, saldo, login_fail])
 
 logger.tulis_csv("data/nasabah.txt", ["userID", "PIN", "Nama", "NamaIbu", "Saldo"], user_data)
 
